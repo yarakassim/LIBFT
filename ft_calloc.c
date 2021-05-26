6c91491560e178ff6bedf6f0aafb21bbc991b179
Yara Kassim <yarakassim@MacBook-Pro-de-Yara.local>
2021-05-26 22:18:43 +0200

    home

--- ft_calloc.c
+++ ft_calloc.c
@@ -11,19 +11,28 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 void *ft_calloc(size_t count, size_t size)
 {
-	void *p;
+	int i;
+	unsigned char *p;
 
+	i = 0;
 	p = malloc(count * size);
-	while (p)
+	while (p[i])
 	{
-		*p = '\0';
-		p++;
-	}
-	return p;
+		p[i] = '\0';
+		i++;
+	} 
+	return (void *)p;
 }
 int main()
 {
-
+	int c;
+	int *t = ft_calloc(5, 4);
+	for(c = 0; c < 5; c++)
+	{
+		printf("%d\n", t[c]);
+	}
+	return 0;
 }
